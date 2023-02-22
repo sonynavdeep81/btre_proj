e0c3750b6fdb2d626cafcfa7b9b47033a7a2e187
sonynavdeep81 <sonynavdeep81@gmail.com>
2023-02-22 14:31:29 +0530

    updated

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from realtor.models import Realtor
+from listings.models import Listing
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings':listings
+    }    
+    return render(request, 'pages/index.html',context)
 
 
 def about(request):
